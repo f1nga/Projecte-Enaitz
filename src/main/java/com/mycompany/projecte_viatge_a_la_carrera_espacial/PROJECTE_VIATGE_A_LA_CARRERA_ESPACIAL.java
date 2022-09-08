ab352ae62a7d966f4b59046a18931a45095aeb20
f1nga <didacgros85@gmail.com>
2022-09-08 21:55:55 +0200

    S'afegeix el projecte

---
+++ src/main/java/com/mycompany/projecte_viatge_a_la_carrera_espacial/PROJECTE_VIATGE_A_LA_CARRERA_ESPACIAL.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.projecte_viatge_a_la_carrera_espacial;
+
+/**
+ *
+ * @author didac
+ */
+public class PROJECTE_VIATGE_A_LA_CARRERA_ESPACIAL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
