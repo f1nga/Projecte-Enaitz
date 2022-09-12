f02987b0b5102e85c9f72f24e4dc6e3409f31a4a
Stanislav2003 <pluvec03@gmail.com>
2022-09-12 13:50:47 +0200

    Mecanic Funcio data reparacio

--- src/main/java/com/mycompany/trabajadores/Astronauta.java
+++ src/main/java/com/mycompany/trabajadores/Astronauta.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.trabajadores;
-
 /**
  *
  * @author Stanislav
@@ -28,27 +27,6 @@ public class Astronauta extends Persona{
     public int getSegons() {
         return segons;
     }
-     
-     /* public static void inserirAstronauta(String nom,String adreca,int edat,int missionsOK,int missionsKO,String rang,String dataVol) throws SQLException, FileNotFoundException {
-
-        try{
-            String sql = "INSERT INTO astronauta VALUES(?,?,?,?,?,?,?)";
-
-            PreparedStatement sentencia =  ConnectorDB.con.prepareStatement(sql);
-            sentencia.setString(1,nom);
-            sentencia.setString(2,adreca);
-            sentencia.setInt(3, edat);
-            sentencia.setInt(4,missionsOK);
-            sentencia.setInt(5,missionsKO);
-            sentencia.setString(6,rang);
-            sentencia.setString(7,dataVol);
-            System.out.println("Contacte inserit");
-            sentencia.executeUpdate();
-        }catch(SQLException e){
-            System.out.println("error fort"+e);
-        }
-    }
-   */
      public void enviarCoordenades(Astronauta As){
          
          graus = (int)(Math.random()*999);
@@ -64,8 +42,6 @@ public class Astronauta extends Persona{
              default -> 'O';
          };
          
-         
          System.out.println("Comunica Astronauta "+As.getNom()+" la seva localització es: "+As.getGraus()+"/"+As.getMinuts()+"/"+As.getSegons()+"["+NSEW+"]");
-         
      }
      }
