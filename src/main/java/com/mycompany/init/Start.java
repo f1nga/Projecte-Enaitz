7a1aa553d3273f3f78e4ee79d8445476a330c9b0
Stanislav2003 <pluvec03@gmail.com>
2022-09-12 11:21:22 +0200

    Funcio localitzacio Astronauta

--- src/main/java/com/mycompany/init/Start.java
+++ src/main/java/com/mycompany/init/Start.java
@@ -1,6 +1,8 @@
 package com.mycompany.init;
 
 import com.mycompany.connection.ConnectorDB;
+import com.mycompany.trabajadores.Astronauta;
+import java.awt.EventQueue;
 import java.sql.SQLException;
 
 /**
@@ -10,6 +12,17 @@ import java.sql.SQLException;
 public class Start {
 
     public static void main(String[] args) throws SQLException {
-        ConnectorDB con = new ConnectorDB();
+        //ConnectorDB con = new ConnectorDB();
+            //con.connectarBaseDades();  
+            
+            /*Astronauta a1 = new Astronauta("Joan");
+            
+            a1.inserirAstronauta(a1.getNom(),"Carrer granota",20,10,0,"militar","data1");*/
+                    
+            Astronauta a1 = new Astronauta("Joan");
+            a1.enviarCoordenades(a1);
     }
-}
+
+
+    }
+
--- src/main/java/com/mycompany/trabajadores/Astronauta.java
+++ src/main/java/com/mycompany/trabajadores/Astronauta.java
@@ -4,35 +4,37 @@
  */
 package com.mycompany.trabajadores;
 
-import com.mycompany.connection.ConnectorDB;
-import java.awt.EventQueue;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 /**
  *
- * @author didac
+ * @author Stanislav
  */
-
 public class Astronauta extends Persona{
      public Astronauta(String nom) {
         super(nom);
     }
      private int missionsOK;
      private String rangMilitar;
+     private int graus;
+     private int minuts;
+     private int segons;
+     private int NSEW;
      
+     public int getGraus() {
+        return graus;
+    }
+    public int getMinuts() {
+        return minuts;
+    }
+    public int getSegons() {
+        return segons;
+    }
      
-      public static void inserirAstronauta(String nom,String adreca,int edat,int missionsOK,int missionsKO,String rang,String dataVol) throws SQLException, FileNotFoundException {
-        
-        ConnectorDB conex = new ConnectorDB();
-        conex.connectarBaseDades();  
+     /* public static void inserirAstronauta(String nom,String adreca,int edat,int missionsOK,int missionsKO,String rang,String dataVol) throws SQLException, FileNotFoundException {
+
         try{
             String sql = "INSERT INTO astronauta VALUES(?,?,?,?,?,?,?)";
 
-            PreparedStatement sentencia = conex.con.prepareStatement(sql);
+            PreparedStatement sentencia =  ConnectorDB.con.prepareStatement(sql);
             sentencia.setString(1,nom);
             sentencia.setString(2,adreca);
             sentencia.setInt(3, edat);
@@ -46,9 +48,24 @@ public class Astronauta extends Persona{
             System.out.println("error fort"+e);
         }
     }
-   
-    
-    
-   
-      
-}
+   */
+     public void enviarCoordenades(Astronauta As){
+         
+         graus = (int)(Math.random()*999);
+         minuts = (int)(Math.random()*99);
+         segons = (int)(Math.random()*59);
+         
+         int localNSEW = (int)(Math.random()*3);
+         
+         NSEW = switch (localNSEW) {
+             case 0 -> 'N';
+             case 1 -> 'S';
+             case 2 -> 'E';
+             default -> 'O';
+         };
+         
+         
+         System.out.println("Comunica Astronauta "+As.getNom()+" la seva localització es: "+As.getGraus()+"/"+As.getMinuts()+"/"+As.getSegons()+"["+NSEW+"]");
+         
+     }
+     }
